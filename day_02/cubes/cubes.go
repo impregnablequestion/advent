23ef79ada2759c33cc531390e6869f292d70b760
Séamus Ryan <ryans40@tcd.ie>
2023-12-02 13:58:43 +0000

    Finished day 2

--- day_02/cubes/cubes.go
+++ day_02/cubes/cubes.go
@@ -6,26 +6,20 @@ import (
     "strconv"
 )
 
-const RedMax uint = 12;
-const GreenMax uint = 13;
-const BlueMax uint = 14;
-
 func Cube(game string) (uint, error) {
-    // Get the ID and the content 
+    // Remove the ID and the content 
     split := strings.Split(game, ": ")
-    strId := strings.Replace(split[0], "Game ", "", 1)
-    id, err := strconv.Atoi(strId)
-    if err != nil {
-        log.Fatalf("Error parsing id: %v", err)
-        return 0, err
-    }
     game = split[1]
+
+    var redMin uint = 0;
+    var blueMin uint = 0;
+    var greenMin uint = 0;
 
     // Get the sessions
 
     sessions := strings.Split(game, "; ");
     for _, session := range sessions {
-        log.Printf(session)
+
         cubes := strings.Split(session, ", ")
 
         for _, cube := range cubes {
@@ -34,27 +28,28 @@ func Cube(game string) (uint, error) {
             i, err := strconv.Atoi(temp[0])
             if err != nil {
                 log.Fatalf("Error parsing number: %v", err)
-                return uint(id), err
+                return 0, err
             }
 
             number := uint(i)
 
             switch color := temp[1]; color {
             case "red":
-                if (number > RedMax) {
-                    return 0, err
+                if (number > redMin) {
+                    redMin = number
                 }
             case "blue":
-                if (number > BlueMax) {
-                    return 0, err
+                if (number > blueMin) {
+                    blueMin = number 
                 }
             case "green":
-                if (number > GreenMax) {
-                    return 0, err
+                if (number > greenMin) {
+                    greenMin = number
                 }
             }
         }
     }
 
-    return uint(id), err
+    power := redMin * blueMin * greenMin 
+    return power, nil 
 }
--- day_02/cubes/cubes_test.go
+++ day_02/cubes/cubes_test.go
@@ -11,23 +11,23 @@ func TestMain(t *testing.T) {
     tests := []test {
         {
             input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-            want: 1,
+            want: 48,
         },
         {
             input: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-            want: 2,
+            want: 12,
         },
         {
             input: "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-            want: 0,
+            want: 1560,
         },
         {
             input: "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-            want: 0,
+            want: 630,
         },
         {
             input: "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-            want: 5,
+            want: 36,
         },
     }
 
